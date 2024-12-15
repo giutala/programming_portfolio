ed5314cc23b48a49be51b6b848e65bbfb6091153
Marek S. Lukasiewicz <marek@lukasiewicz.tech>
2024-12-15 15:49:28 +0100

    Fix bunding firstpic.jpg

--- src/components/CatIllustration.tsx
+++ src/components/CatIllustration.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import firstpicUrl from '../../assets/firstpic.jpg'
 
 export function CatIllustration() {
   return (
     <div className="relative w-full h-[300px] md:h-[400px] lg:h-[500px]">
-      <img 
-        src="assets\firstpic.jpg" 
-        alt="Illustration of a cat" 
+      <img
+        src={firstpicUrl}
+        alt="An illustration of a woman working with digital data"
         className="w-full h-full object-cover"
       />
     </div>
